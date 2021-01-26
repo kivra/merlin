9cab7143b7fcf58d19f9e70b99292055dd0f7a71
Max Nordlund kivra <max.nordlund@kivra.com>
2021-01-26 18:30:16 +0100

    Implement the hygienic meta macro

--- include/merlin_macros.hrl
+++ include/merlin_macros.hrl
@@ -31,8 +31,29 @@
     erl_parse:abstract(Expression, ?LINE)
 )).
 
--define(hygienic(MACRO, BODY), ?procedural(hygienic(MACRO, BODY),
-    BODY % Implement hygine, aka rename variables
-)).
+-define(hygienic(MACRO, BODY), ?procedural(hygienic(MACRO, BODY), begin
+    [Body] = abstract(BODY),
+    Variables = erl_syntax_lib:variables(Body),
+    VariableMapping = maps:from_list(lists:zip(
+        sets:to_list(Variables),
+        merlin_lib:new_variables(Variables, sets:size(Variables))
+    )),
+    {[Form], _} = merlin:transform(
+        [Body],
+        fun(leaf, Node, Map) ->
+            case erl_syntax:type(Node) =:= variable of
+                false -> continue;
+                true ->
+                    Name = erl_syntax:variable_name(Node),
+                    NewName = maps:get(Name, Map, Name),
+                    NewVariable = erl_syntax:variable(NewName),
+                    erl_syntax:copy_attrs(Node, NewVariable)
+            end;
+            (_, _, _) -> continue
+        end,
+        VariableMapping
+    ),
+    Form
+end)).
 
 -endif.
